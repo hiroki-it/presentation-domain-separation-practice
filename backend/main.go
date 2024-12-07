debb9dffba768562dfa281d43a293fda29336b92
hiroki-it <hasegawafeedshop@gmail.com>
2024-12-08 04:31:11 +0900

    CORSに対処した

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,7 +9,7 @@ func main() {
 	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		resp := []byte(`{"status": "ok"}`)
 		rw.Header().Set("Content-Type", "application/json")
-		rw.Header().Set("Content-Length", fmt.Sprint(len(resp)))
+		rw.Header().Set("Access-Control-Allow-Origin", "*")
 		rw.Write(resp)
 	})
 
