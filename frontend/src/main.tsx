cd2172d22d2b405fb23e62ee30a54a958feec750
hiroki-it <hasegawafeedshop@gmail.com>
2024-12-09 17:54:23 +0900

    IDプロバイダーに認可リクエストを送信できるようにした

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,19 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {App} from './App.tsx'
+import {AuthProvider} from "react-oidc-context";
+
+const oidcConfig = {
+  authority: "http://localhost:8080/realms/authentication-service",
+  client_id: "services",
+  scope: "openid profile email offline_access",
+  redirect_uri: window.location.origin + "http://localhost:5173/authentication/callback",
+};
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+      <AuthProvider {...oidcConfig}>
         <App/>
+      </AuthProvider>
     </StrictMode>,
 )
