42736f9e05a26c6fe377328ef9b1acfe3f7db846
hiroki-it <hasegawafeedshop@gmail.com>
2024-12-09 20:21:24 +0900

    クライアントIDを変更した

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,8 @@ import {App} from './App.tsx'
 import {AuthProvider} from "react-oidc-context";
 
 const oidcConfig = {
-  authority: "http://localhost:8080/realms/authentication-service",
-  client_id: "services",
+  authority: "http://localhost:8080/realms/development",
+  client_id: "frontend",
   scope: "openid profile email offline_access",
   redirect_uri: window.location.origin + "/authentication/callback",
 };
