c4077dc76e18fe7c631d9edb42b611f018ec7003
hiroki-it <hasegawafeedshop@gmail.com>
2024-12-09 19:03:45 +0900

    リダイレクトURLを修正した

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ const oidcConfig = {
   authority: "http://localhost:8080/realms/authentication-service",
   client_id: "services",
   scope: "openid profile email offline_access",
-  redirect_uri: window.location.origin + "http://localhost:5173/authentication/callback",
+  redirect_uri: window.location.origin + "/authentication/callback",
 };
 
 createRoot(document.getElementById('root')!).render(
